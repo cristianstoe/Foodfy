245ade9102f479073795777ae8d2c8e82963bfbe
Cristian Stoenica <cristian3300@gmail.com>
2020-10-28 11:41:59 -0300

    added dynamic fields for form

--- public/scripts.js
+++ public/scripts.js
@@ -26,3 +26,41 @@ for(let card of cards){
         }
     })
 }
+
+function addIngredient() {
+    const ingredients = document.querySelector("#item");
+    const fieldContainer = document.querySelectorAll(".recipeIngredients");
+  
+    // Realiza um clone do último ingrediente adicionado
+    const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
+  
+    // Não adiciona um novo input se o último tem um valor vazio
+    if (newField.children[0].value == "") return false;
+  
+    // Deixa o valor do input vazio
+    newField.children[0].value = "";
+    ingredients.appendChild(newField);
+  }
+  
+  document
+    .querySelector(".add-ingredient")
+    .addEventListener("click", addIngredient);
+
+function addStep() {
+        const ingredients = document.querySelector("#itemPrep");
+        const fieldContainer = document.querySelectorAll(".recipeSteps");
+      
+        // Realiza um clone do último ingrediente adicionado
+        const newField = fieldContainer[fieldContainer.length - 1].cloneNode(true);
+      
+        // Não adiciona um novo input se o último tem um valor vazio
+        if (newField.children[0].value == "") return false;
+      
+        // Deixa o valor do input vazio
+        newField.children[0].value = "";
+        ingredients.appendChild(newField);
+      }
+      
+      document
+        .querySelector(".add-step")
+        .addEventListener("click", addStep);
